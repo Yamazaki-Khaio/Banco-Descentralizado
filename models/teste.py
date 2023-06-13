bd4a2ed752801d53961d0fad1019540b35692eac
Yamazaki Khaio <kfonseca@ecomp.uefs.br>
2023-06-13 11:05:03 -0300

    :sparkles: feat: testcase

--- models/teste.py
+++ models/teste.py
@@ -1,45 +1,60 @@
+import unittest
+import threading
+from flask import Flask, json, jsonify, request
+from api import app
 
-import requests
+# Simulando a estrutura de dados 'contas'
+contas = {
+    'conta_origem_id': 100,
+    'conta_destino_id': 50
+}
 
-def test_routes(url1, url2):
-    # Cria uma nova conta na instância 1
-    create_account_url1 = f'{url1}/create_account'
-    data = {'account_number': '123', 'initial_balance': 100}
-    response = requests.post(create_account_url1, json=data)
-    print(response.json())
+# Simulando as funções bakery_lock e bakery_unlock
+def bakery_lock(conta_id):
+    pass
 
-    # Faz um depósito na conta criada na instância 1
-    deposit_url1 = f'{url1}/deposit'
-    data = {'account_number': '123', 'amount': 50}
-    response = requests.post(deposit_url1, json=data)
-    print(response.json())
+def bakery_unlock(conta_id):
+    pass
 
-    # Obtém o saldo da conta criada na instância 1
-    balance_url1 = f'{url1}/balance/123'
-    response = requests.get(balance_url1)
-    print(response.json())
+class TransferenciaConcorrenciaTestCase(unittest.TestCase):
 
-    # Obtém todas as contas da instância 1
-    accounts_url1 = f'{url1}/accounts'
-    response = requests.get(accounts_url1)
-    print(response.json())
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
 
-    # Obtém todas as contas da instância 2
-    accounts_url2 = f'{url2}/accounts'
-    response = requests.get(accounts_url2)
-    print(response.json())
+    def test_transferencia_concorrencia(self):
+        # Define o número de solicitações concorrentes
+        num_solicitacoes = 10
 
-    # Faz uma requisição para obter os dados de todas as contas na instância 1
-    accounts_from_server1_url2 = f'{url2}/accounts_from_server1'
-    response = requests.get(accounts_from_server1_url2)
-    print(response.json())
+        # Define os dados da transferência
+        payload = {
+            'valor': 10,
+            'conta_destino': 'conta_destino_id'
+        }
 
+        # Lista para armazenar as threads
+        threads = []
 
+        # Função que será executada por cada thread
+        def realizar_transferencia():
+            response = self.app.post('/contas/conta_origem_id/transferencia', json=payload)
+            data = json.loads(response.get_data(as_text=True))
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(data['sucesso'])
+            self.assertEqual(data['mensagem'], 'Transferência de 10 para a conta conta_destino_id realizada')
+
+        # Cria as threads e as adiciona à lista
+        for _ in range(num_solicitacoes):
+            thread = threading.Thread(target=realizar_transferencia)
+            threads.append(thread)
+
+        # Inicia todas as threads
+        for thread in threads:
+            thread.start()
+
+        # Aguarda a conclusão de todas as threads
+        for thread in threads:
+            thread.join()
 
 if __name__ == '__main__':
-    # Define as URLs das instâncias do servidor Flask
-    url1 = 'http://localhost:51636'  # URL da primeira instância
-    url2 = 'http://localhost:51649'  # URL da segunda instância
-
-    # Executa o teste das rotas
-    test_routes(url1, url2)
+    unittest.main()
